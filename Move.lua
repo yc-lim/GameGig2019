45d11be0cb9be2c1894d108d3f9eb9455f647f12
Boyan Yu <by261@cam.ac.uk>
2019-12-06 13:48:50 +0000

    Added move

---
+++ Move.lua
@@ -0,0 +1,47 @@
+--initialization
+
+FuelTime = 0
+HP = 100
+Damage1 = 32
+Damage2 = 25
+MaxFuel = 100
+Speed = 15
+angle = 0
+
+function damage(a)
+  HP = HP - a
+end
+
+function move()
+    local speed = 200
+    --the current
+    local velocity
+    --in radians per sec!
+    local angularV
+    local vx = 0
+    local vy = 0
+
+    -- get input
+    if love.keyboard.isDown('up') then
+        velocity = speed
+    elseif love.keyboard.isDown('down') then
+        velocity = -speed
+    end
+
+    if love.keyboard.isDown('left') then
+        angle = angle - angularV
+    elseif love.keyboard.isDown('right') then
+        angle = angle + angularV
+    end
+
+    -- calculate speed
+    local v = math.sqrt(vx^2 + vy^2)
+    if v > speed then
+        vx = velocity*math.sin(angle)
+        vy = velocity*math.cos(angle)
+    end
+
+    -- update position
+    self.x = self.x + vx * dt
+    self.y = self.y + vy * dt
+end
