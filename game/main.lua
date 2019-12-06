cfc76a2814d2917786bf5e0b9e3aa41bd29e5787
yc-lim <47224937+ycl356@users.noreply.github.com>
2019-12-06 14:39:10 +0000

    .

--- game/main.lua
+++ game/main.lua
@@ -1,4 +1,5 @@
-
+local class = require 'middleclass'
+local class = require 'Player'
 
 function love.load()
   screenW = 1280
@@ -8,26 +9,29 @@ function love.load()
   -- love.graphics.setCanvas(canvas)
 
   startX = 20
+  
+  p1 = Player:new("P1")
+  p2 = Player:new("P2")
 
-  images = {}
-  images["P1"] =  love.graphics.newImage("assets/tank.png")
-  images["P2"] = love.graphics.newImage("assets/tank.png")
+  --images = {}
+  --images["P1"] =  love.graphics.newImage("assets/tank.png")
+  --images["P2"] = love.graphics.newImage("assets/tank.png")
 
-  p1 = {}
+  --[[p1 = {}
   p1.name = "P1"
   p1.image = love.graphics.newImage("assets/tank.png")
   p1.w , p1.h = p1.image:getDimensions()
   p1.x = startX + p1.h/2
   p1.y = screenH/2
-  p1.angle = math.pi/2
+  p1.angle = math.pi/2 ]]
 
-  p2 = {}
+  --[[p2 = {}
   p2.name = "P2"
   p2.image = love.graphics.newImage("assets/tank.png")
   p2.w , p2.h = p2.image:getDimensions()
   p2.x = screenW - p2.h/2 - startX
   p2.y = screenH/2
-  p2.angle =  - math.pi/2
+  p2.angle =  - math.pi/2]]
 
 end
 
@@ -44,8 +48,9 @@ function love.draw()
   love.graphics.setColor(200, 200, 200)
   love.graphics.rectangle("fill", 0, 0, screenW, screenH)
   
-  drawPlayer(p1)
-  drawPlayer(p2)
+  p1.draw()
+  p2.draw()
+  --drawPlayer(p2)
   love.graphics.setColor(255, 0, 0)
   love.graphics.circle("fill", screenW/2, screenH/2, 2, 2)
 end
--- game/player.lua
+++ game/player.lua
@@ -1,4 +1,63 @@
-local class = require 'middleclass.lua'
+local class = require 'middleclass'
 
 Player = class('Player')
+
+Damage1 = 32
+Damage2 = 25
+MaxFuel = 100
+
+function Player:initialize(name)
+  self.name = name
+  self.image = love.graphics.newImage("assets/tank.png")
+  self.w , self.h = self.image:getDimensions()
+  self.x = startX + self.h/2
+  self.y = screenH/2
+  self.angle = math.pi/2
+  print(30)
+  -- self.FuelTime = 0
+  -- self.HP = 100
+  -- self.Speed = 15
+end
+
+function Player:update(dt)
+    local speed = 200
+    --the current
+    local velocity
+    --in radians per sec!
+    local angularV = math.pi/6
+    local vx = 0
+    local vy = 0
+
+    -- get input
+    if love.keyboard.isDown('up') then
+        velocity = speed
+    elseif love.keyboard.isDown('down') then
+        velocity = -speed
+    end
+
+    if love.keyboard.isDown('left') then
+        angle = angle - angularV
+    elseif love.keyboard.isDown('right') then
+        angle = angle + angularV
+    end
+
+    -- calculate speed
+    local v = math.sqrt(vx^2 + vy^2)
+    if v > speed then
+        vx = velocity*math.sin(angle)
+        vy = velocity*math.cos(angle)
+    end
+
+    -- update position
+    self.x = self.x + vx * dt
+    self.y = self.y + vy * dt
+end
+
+function Player:draw()
+  love.graphics.draw(self.image, self.x, self.y, self.angle, 1, 1, self.w/2, self.h/2)
+end
+
+
+
+
 
